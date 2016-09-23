1d95cba5d1a562d84d8c83a6426e8147da834503
Brunux <bruno.fosados@gmail.com>
2016-09-23 00:28:58 +0000

    Add labeles and widgets to create-streming form

--- streamingapp/forms.py
+++ streamingapp/forms.py
@@ -1,10 +1,29 @@
 # Forms
-
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
 from django import forms
 from .models import Streaming
+import uuid
 
 class StreamingForm(forms.ModelForm):
     class Meta:
         model = Streaming
-        fields = ['title', 'init_date', 'init_time', 'info']
+        fields = ['title', 'init_date', 'init_time', 'duration', 'info', 'is_public']
+        widgets = {
+            'title': forms.Textarea(attrs={'cols': 100, 'rows': 1}),
+            'info': forms.Textarea(attrs={'cols': 100, 'rows': 5}),
+        }
+        labels = {
+            'title': _('Titulo'),
+            'init_date': _('Fecha de inicio'),
+            'init_time': _('Hora de inicio'),
+            'duration': _('Duracion'),
+            'info': _('Descripcion'),
+            'is_public': _('Es publico?'),
+        }
+        error_messages = {
+            'title': {
+                'max_length': _("El titulo es demasiado largo. Max 100 caracteres"),
+            },
+        }
         # Check Streaming model for all the fields needed to save the form
--- streamingapp/models.py
+++ streamingapp/models.py
@@ -3,12 +3,27 @@ from django.db import models
 # Create your models here.
 
 class Streaming(models.Model):
+    ONE = '1'
+    HALF = '30'
+    QUARTER = '15'
+    DURATION_CHOICES = (
+        (ONE, "1 Hora"),
+        (HALF, "30 Minutos"),
+        (QUARTER, "15 Minutos"),
+    )
+    
     user = models.CharField(max_length=128)
     title = models.CharField(max_length=100)
     init_date = models.DateField(auto_now=False, auto_now_add=False)
     init_time = models.TimeField(auto_now=False, auto_now_add=False)
+    duration = models.CharField(
+        max_length=2,
+        choices=DURATION_CHOICES,
+        default=HALF,
+    )
     uuid = models.UUIDField()
     info = models.TextField()
+    is_public = models.BooleanField(default=True)
     # Add presentation field as optional
     
     def __str__(self):
@@ -16,6 +31,6 @@ class Streaming(models.Model):
 
 # import uuid, datetime
 # from streamingapp.models import Streaming
-# stm = Streaming(user='bruno@virtuososcode.com', title='Meetup 12', init_date=datetime.date.today(), init_time=datetime.datetime.now().time(), uuid=uuid.uuid4(), info='Este es un streaming de prueba')
+# stm = Streaming(user='bruno@virtuososcode.com', title='Meetup 12', init_date=datetime.date.today(), init_time=datetime.datetime.now().time(), uuid=uuid.uuid4(), info='Este es un streaming de prueba' is_public=True)
 # time field >>> datetime.datetime.now().time()
 # datetime.time(10, 22, 33, 999000)
